157f127e5cbfcf3e21959d9cd3db87ae752cb2fa
loscar1223-alt <loscar1223@gmail.com>
2026-04-17 21:45:00 -0500

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -20,7 +20,7 @@ from pathlib import Path
 
 import defusedxml.minidom
 
-from helpers.merge_runs import merge_runs as do_merge_runs
+
 from helpers.simplify_redlines import simplify_redlines as do_simplify_redlines
 
 SMART_QUOTE_REPLACEMENTS = {
